1ea340e3ad54982d3126ba681ec90931585a2723
imsyf <29587914+imsyf@users.noreply.github.com>
2025-09-27 18:53:24 +0700

    Initialize new project

---
+++ tca-tutorial/tca_tutorialApp.swift
@@ -0,0 +1,16 @@
+//
+//  tca_tutorialApp.swift
+//  tca-tutorial
+//
+//  Created by Imam on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct tca_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
