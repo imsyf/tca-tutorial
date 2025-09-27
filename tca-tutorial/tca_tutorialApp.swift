c20e613a61c871b8e975e371ead6ed7c06b17a10
imsyf <29587914+imsyf@users.noreply.github.com>
2025-09-27 18:54:21 +0700

    Integrate into the app

--- tca-tutorial/tca_tutorialApp.swift
+++ tca-tutorial/tca_tutorialApp.swift
@@ -5,12 +5,19 @@
 //  Created by Imam on 27/09/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct tca_tutorialApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+
     var body: some Scene {
         WindowGroup {
+            CounterView(store: tca_tutorialApp.store)
         }
     }
 }
