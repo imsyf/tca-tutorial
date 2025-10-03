32c875df334177c489eebe5c94ca8266cbe4992f
imsyf <29587914+imsyf@users.noreply.github.com>
2025-10-04 06:49:31 +0700

    Compose reducers

--- CounterFeatureTests/CounterFeatureTests.swift
+++ CounterFeatureTests/CounterFeatureTests.swift
@@ -65,4 +65,14 @@ struct CounterFeatureTests {
             $0.fact = "0 is a good number."
         }
     }
+    
+    @Test func incrementInFirstTab() async {
+        let store = TestStore(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+        
+        await store.send(\.tab1.incrementButtonTapped) {
+            $0.tab1.count = 1
+        }
+    }
 }
---
+++ tca-tutorial/AppFeature.swift
@@ -0,0 +1,37 @@
+//
+//  AppFeature.swift
+//  tca-tutorial
+//
+//  Created by Imam on 02/10/25.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct AppFeature {
+    struct State: Equatable {
+        var tab1 = CounterFeature.State()
+        var tab2 = CounterFeature.State()
+    }
+    
+    enum Action {
+        case tab1(CounterFeature.Action)
+        case tab2(CounterFeature.Action)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Scope(state: \.tab1, action: \.tab1) {
+            CounterFeature()
+        }
+        
+        Scope(state: \.tab2, action: \.tab2) {
+            CounterFeature()
+        }
+
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
+
